f247b0c86eda911af86359492f05019ff1ec1fe8
Tommaso Morganti <tommaso.morganti01@gmail.com>
2025-02-26 23:54:59 +0100

    fix: edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/polinetworkorg/polinetworkdocs',
+            'https://github.com/polinetworkorg/polinetworkdocs/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
