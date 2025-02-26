00653a6d0918b68f0e84c6b3ecceceee0ce1bf0e
Tommaso Morganti <tommaso.morganti01@gmail.com>
2025-02-27 00:38:08 +0100

    chore: updated the whole thing

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import prettier from 'eslint-config-prettier'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['src/**/*.{ts,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
+  prettier,
+  {
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off', // for requiring svgs
+    },
+  },
+  { ignores: ['**/.docusaurus/**'] },
+]
