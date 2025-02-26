f3d9ebad83148037fb70a43baacc5ebab385115d
Tommaso Morganti <tommaso.morganti01@gmail.com>
2025-02-26 23:44:53 +0100

    i do be stoopid, copypasting hardest job in IT

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/polinetworkorg/polinetworkdocs',
+            'https://github.com/polinetworkorg/polinetworkdocs/tree/master',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -73,7 +73,7 @@ const config = {
             label: 'Infra',
           },
           {
-            href: 'https://github.com/facebook/docusaurus/tree/master',
+            href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
             position: 'right',
           },
